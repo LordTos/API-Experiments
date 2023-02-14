a4c0d022acba64643afda944281e03de3603de3a
Thiago de Oliveira Sant' Anna <thiagotos33@gmail.com>
2023-02-14 09:35:27 -0300

    criação env

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World UwU');
+
+// Atividade comentar cada linha do código, resumo de cada linha do código.
+//Comentátio aqui
+const express = require('express')
+
+const conf = require('dotenv').config().parsed;
+console.log(conf);
+
+//Comentátio aqui
+const app = express()
+
+//Comentátio aqui
+const port = 3000
+
+//Comentátio aqui
+app.get('/', (req,res) => {
+
+    //Comentátio aqui
+    res.send('Biel next instrutor')
+})
+
+//Comentátio aqui
+app.listen(port, () => {
+
+    //Comentátio aqui
+    console.log(`Example app listening on port ${port}`)
+})
