00450f3e8dcc6e57dabd06a377642d436aefdc3c
Thiago de Oliveira Sant' Anna <thiagotos33@gmail.com>
2023-02-15 09:14:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,30 @@ console.log(conf);
 const app = express()
 
 //Comentátio aqui
-const port = 3000
+const port = conf.HTTPPORT;
+
+//Objeto que será executado quando houver uma requisição.
+const controller = {
+    ressJson: async (req, res) =>{
+
+        //Lista com alguns atributos uteis da requisição (req) HTTP.
+        data = {
+            "method": req.method,
+            "url": req.url,
+            "baseURL": req.baseURL,
+            "query": res.query,
+            "params": req.params,
+            "body":req.body,
+            "headers": req.headers
+        }
+
+        //Envia JSON com os dados acima para o cliente, como texto plano.
+        // res.send(data);
+
+        //Envia JSON com os dados acima para o cliente, como texto plano JSON.
+        res.json(data);
+    }
+}
 
 //Comentátio aqui
 app.get('/', (req,res) => {
