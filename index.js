cc3741c1983001df59330cb5532bd1c625f077ef
Thiago de Oliveira Sant' Anna <thiagotos33@gmail.com>
2023-02-17 11:02:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const conf = require('dotenv').config().parsed;
 //console.log(conf);
 
 //Importa a biblioteca "MySQL"
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 
 //Faz a conexão com o servidor.
 const conn = mysql.createPool({
